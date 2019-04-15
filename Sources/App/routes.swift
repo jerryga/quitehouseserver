e3bde9203639563eb3469dba457ae8719060b3f1
JiaChuansheng <jiachuansheng@baidu.com>
2019-04-15 20:56:45 +0800

    congra

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        return "Congratulations to Xiao Erge！🎉🎉🎉"
     }
     
     // Basic "Hello, world!" example
