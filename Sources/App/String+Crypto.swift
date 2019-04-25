6194c073b0c0c0816272e5b5ec8fd5dcb6c33465
JiaChuansheng <jiachuansheng@baidu.com>
2019-04-25 11:12:40 +0800

    hexEncodedString[C

--- Sources/App/String+Crypto.swift
+++ Sources/App/String+Crypto.swift
@@ -7,23 +7,17 @@
 //
 
 import Foundation
-import CommonCrypto
+import Crypto
 
 extension String {
     func MD5() -> String {
-        let str = self.cString(using: String.Encoding.utf8)
-        let strLen = CC_LONG(self.lengthOfBytes(using: String.Encoding.utf8))
-        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
-        let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
         
-        CC_MD5(str!, strLen, result)
-        
-        let hash = NSMutableString()
-        for i in 0 ..< digestLen {
-            hash.appendFormat("%02x", result[i])
+        let digest = Digest.init(algorithm: .md5)
+        do {
+            let result = try digest.hash(self)
+            return result.hexEncodedString()
+        }catch {
+            return ""
         }
-        result.deallocate()
-        
-        return String(format: hash as String)
     }
 }
